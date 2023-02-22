5881bc655dc92a8224d9234798495812d49fa2d1
jpmm21 <jmedinamulet@gmail.com>
2023-02-22 19:58:50 +0200

    api get implemented, style fixed

--- main.js
+++ main.js
@@ -3,13 +3,38 @@ const searchInput = document.getElementById("search-input")
 const movieFeed = document.getElementById('movie-feed')
 
 searchBtn.addEventListener("click", async function () {
-    let res = fetch(`http://www.omdbapi.com/?apikey=84d8cf6b&s=${searchInput.value}`)
-    let data = await (await res).json()
-    console.log(data.Search)
-    // renderMovies(data)
+    let res = await fetch(`http://www.omdbapi.com/?apikey=84d8cf6b&s=${searchInput.value}`)
+    let data = await res.json()
+    getMovieList(data.Search)
 })
 
-// function renderMovies(data) {
-//     let movieHtml = ''
-//     data
-// }
+async function getMovieList(data) {
+   let movieList = []
+    for(let i = 0; i < data.length; i++){
+        let res = await fetch(`http://www.omdbapi.com/?apikey=84d8cf6b&i=${data[i].imdbID}`)
+        let movieDetails = await res.json()
+        movieList.push(movieDetails)
+    }
+    renderMovies(movieList)
+}
+
+function renderMovies(movieList){
+    let movieHtml = ''
+    movieList.forEach(movieData => {
+        movieHtml += `
+            <div class="movie">
+                <img class="movie-poster" src="${movieData.Poster}" alt="Poster for ${movieData.Title}">
+                <div class="movie-info">
+                    <h3>${movieData.Title}</h3>
+                    <div class="movie-details">
+                        <p class="info-el">${movieData.Runtime}</p>
+                        <p class="info-el">${movieData.Genre}</p>
+                        <img src="images/plus.svg" alt="add to watchlist button">
+                    </div>
+                    <p class="movie-description info-el">${movieData.Plot}</p>
+                </div>
+            </div>`
+    });
+    movieFeed.innerHTML = movieHtml
+    movieFeed.style.backgroundImage = "none"
+}
